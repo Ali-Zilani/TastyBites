32e27fd9a92bb36c5688c610114856ba5e7ba565
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2024-07-04 16:37:43 +0530

    Frontend Setup , Installed React and intergrated tailwind Css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-center text-5xl text-gray-600 py-10">'بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ'</h1>
+    </>
+  )
+}
+
+export default App
